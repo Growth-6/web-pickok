67795f19a6cf32ec94a7a51bab5edf42a3a22d89
yujinyny <hyjinny00@gmail.com>
2024-07-15 18:36:48 +0900

    chore: zustand 예시 추가

---
+++ components/Counter.tsx
@@ -0,0 +1,12 @@
+import { useStore } from '../hooks/useStore'
+
+export const Counter = () => {
+  const { count, increment } = useStore()
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
